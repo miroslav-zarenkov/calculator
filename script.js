cf9a9c7b70248cc4f760a1722c9609377272a46a
Miroslav Zarenkov <miroslav.zarenkov@gmail.com>
2021-09-24 17:34:18 +0300

    Add basic math functions

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(a, b) {
+    return c = a + b;
+}
+
+function subtract(a, b) {
+    return c = a - b;
+}
+
+function multiply(a, b) {
+    return c = a * b;
+}
+
+function divide(a, b) {
+    return c = a / b;
+}
+
+function operate(operator, a, b) {
+    operator(a, b);
+}
