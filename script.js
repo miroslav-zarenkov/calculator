f87fe245914cb86814c2105a0fa021d38805100a
Miroslav Zarenkov <miroslav.zarenkov@gmail.com>
2021-09-27 18:36:39 +0300

    Trying some stuff, this is obviously wrong approach

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+let inputBox = document.querySelector("#input-box");
+let clearButton = document.querySelector("#clear");
+let reverseButton = document.querySelector("#reverse");
+let divideButton = document.querySelector("#divide");
+
+clearButton.addEventListener("click", clearInputBox);
+reverseButton.addEventListener("click", reverseInputBox);
+divideButton.addEventListener("click", divideInputBox);
+
+
+inputBox.value = "";
+inputBox.innerHTML = inputBox.value;
+
 function add(a, b) {
     return c = a + b;
 }
@@ -14,6 +27,46 @@ function divide(a, b) {
     return c = a / b;
 }
 
+function percentage(a) {
+    return a = a / 100;
+}
+
+function reverse(a) {
+    return a = a * -1;
+}
+
+function comma(a) {
+    return a = parseInt(a + ",");
+}
+
 function operate(operator, a, b) {
     operator(a, b);
+}
+
+function clearInputBox() {
+    document.querySelector("#input-box").value = "";
+    inputBox.innerHTML = inputBox.value;
+}
+
+function reverseInputBox() {
+    inputBox.value = inputBox.value * -1;
+    inputBox.innerHTML = inputBox.value;
+}
+
+function divideInputBox() {
+    if (inputBox.innerHTML[inputBox.innerHTML.length - 1] == "/") {
+        return
+    } else
+        inputBox.value = inputBox.value + "/";
+    inputBox.innerHTML = inputBox.value;
+}
+
+function nine() {
+    inputBox.value = inputBox.value + "9";
+    inputBox.innerHTML = inputBox.value;
+}
+
+function parse(str) {
+    inputBox.value = Function(`'use strict'; return (${str})`)();
+    inputBox.innerHTML = inputBox.value;
 }
